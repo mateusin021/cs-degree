d8ac8f8f461ae5f6ab4dc104df0d5098e667a98d
mateus <64480742+mateusin021@users.noreply.github.com>
2023-03-30 10:42:34 -0300

    Add files via upload

---
+++ CTC4002/aula de funcoes/ex04.py
@@ -0,0 +1,15 @@
+from math import pi
+
+def volEsfera(r):
+    return (4/3) * r * pi
+
+def volCubo(a):
+    return a**3
+
+def vazio(cubo, esfera):
+    return cubo - esfera
+
+r = float(input('insira o raio da esfera: '))
+a = float(input('insira a aresta do cubo: '))
+
+print(f'o espaco vazio no dado cubo é: {vazio(volCubo(a),volEsfera(r)):.2f}')
