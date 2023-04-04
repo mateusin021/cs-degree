042cc30cfd9324e9e901d0953a4cf74951575577
mateus <64480742+mateusin021@users.noreply.github.com>
2023-04-04 10:36:44 -0300

    Update ex2.py

--- CTC4002/Digito_Verificador/ex2.py
+++ CTC4002/Digito_Verificador/ex2.py
@@ -1,17 +1,12 @@
 import math
 
 def calc_dv_int(digitos):
-    a,b,c,d = separa_inteiros(digitos)
-    return(4*a+int(str(b)+str(c))+d)%8
+    a = digitos //1000
+    bc =(digitos//10)%100
+    d = digitos %10
+    return(4*a+bc+d)%8
 def numero_conta_completo_int(digitos):
     return int(f'{digitos}{calc_dv_int(digitos)}')
 
-def separa_inteiros(an_int):
-    x = math.log(an_int, 10)
-    y = math.ceil(x)
-
-    a,b,c,d = [(an_int//(10**i)) % 10 for i in range(y, -1, -1)][bool(math.log(an_int, 10) % 1):]
-
-    return a,b,c,d
 digitos = int(input('insira os quatro digitos da conta: '))
 print(f'digito verificador: {calc_dv_int(digitos)}\nnumero da conta: {numero_conta_completo_int(digitos)}')
